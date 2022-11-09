cf0d07ff1d136bda84bd635387ad30ca37294225
向阳 <xiangqian_liu@163.com>
2022-11-09 14:38:26 +0800

    refactor: 重构

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,89 @@
 import { createPatch, applyPatch } from 'diff'
 
 export class CodingTimeline {
-  private historyList: string[] = []
-  private redoStack: string[] = []
   private newData: string = ''
-  private redoValueStack: string[] = []
+  private undoPatchStack: string[] = []
+  private redoPatchStack: string[] = []
+  private undoProduct: string | null = null
+  private redoProduct: string | null = null
+  private point: number = 0
 
   /**
    * 撤销
    */
   undo(): string | boolean {
-    if (this.historyList.length === 0) return false
-    const lastHistory = this.historyList.pop()!
-    this.redoStack.push(lastHistory)
-    this.redoValueStack.push(this.newData)
-    const prevVersion = applyPatch(this.newData, lastHistory)
-    this.newData = prevVersion
-    return this.newData
+    if (this.undoPatchStack.length === 0) return false
+    const lastUndoPatch = this.undoPatchStack.pop()!
+    this.undoProduct = applyPatch(this.newData, lastUndoPatch)
+    this.point = this.undoPatchStack.length - 1
+    this.newData = this.undoProduct
+    return this.undoProduct
   }
 
   /**
    * 撤销之前的撤销
    */
   redo(): string | boolean {
-    if (this.redoStack.length === 0) return false
-    this.newData = this.redoValueStack.pop()!
-    this.historyList.push(this.redoStack.pop()!)
-    return this.newData
+    if (this.redoPatchStack.length === 0 || !this.undoProduct) return false
+    this.point += 1
+    const redoPatch = this.redoPatchStack[this.point]
+    if (!redoPatch) return false
+    this.undoProduct = this.newData
+    this.redoProduct = applyPatch(this.undoProduct, redoPatch)
+    this.undoPatchStack.push(createPatch('diff', this.redoProduct, this.undoProduct))
+    this.newData = this.redoProduct
+    return this.redoProduct
   }
 
   /**
    * 每次再保存之前都要push一下，用来记录操作
    */
   push(newData: string) {
-    const undoPatch = createPatch('diff', newData, this.newData)
+    this.undoPatchStack.push(createPatch('diff', newData, this.newData))
+    this.redoPatchStack.push(createPatch('diff', this.newData, newData))
     this.newData = newData
-    this.historyList.push(undoPatch)
+    if (this.undoPatchStack.length > 100) {
+      // 每当长度超过100的时候就删除栈底元素
+      this.undoPatchStack.shift()
+    }
   }
 
   /**
    * 当当前页面被关闭的时候调用，用来清楚保存的历史操作
    */
   clear() {
-    this.historyList = []
     this.newData = ''
-    this.redoStack = []
-    this.redoValueStack = []
+    this.undoPatchStack = []
+    this.redoPatchStack = []
+    this.undoProduct = null
+    this.redoProduct = null
   }
 }
+
+const codingTimeline = new CodingTimeline()
+codingTimeline.push('aaaa')
+codingTimeline.push('bbbb')
+codingTimeline.push('cccc')
+codingTimeline.push('dddd')
+
+console.log('c', codingTimeline.undo()) // c
+console.log('d', codingTimeline.redo()) // d
+console.log('c', codingTimeline.undo()) // c
+console.log('b', codingTimeline.undo()) // b
+console.log('c', codingTimeline.redo()) // c
+console.log('d', codingTimeline.redo()) // d
+console.log('d', codingTimeline.redo()) // false
+console.log('b', codingTimeline.undo()) // c
+console.log('b', codingTimeline.undo()) // b
+console.log('d', codingTimeline.redo()) // c
+console.log('d', codingTimeline.redo()) // d
+console.log('d', codingTimeline.redo()) // false
+console.log('d', codingTimeline.redo()) // false
+console.log('b', codingTimeline.undo()) // c
+console.log('b', codingTimeline.undo()) // b
+console.log('b', codingTimeline.undo()) // a
+console.log('b', codingTimeline.undo()) // ''
+console.log('b', codingTimeline.undo()) // false
+console.log('b', codingTimeline.undo()) // false
+console.log('b', codingTimeline.undo()) // false
+
